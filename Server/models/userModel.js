14ba03f284f21e8830be2ab431355e4569ec2d11
Sanskriti Khanal <sanskritikhanal@Sanskritis-MacBook-Air.local>
2025-05-23 23:22:24 +0545

    First Commit

---
+++ Server/models/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    verifyotp:{type:String,default:''},
+    verifyotpExpiredAt:{type:Number,default:0},
+    isAccountVerified:{type:Boolean,default:false},
+    resetOtp:{type:String,default:''},
+    resetotpExpiredAt:{type:Number,default:0}
+
+})
+
+const userModel = mongoose.models.user || mongoose.model('user',userSchema);
+
+export default userModel;
+
